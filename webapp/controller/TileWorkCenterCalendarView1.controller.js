a1f28a1fd11fecb56816bcef5a0d034da2ca3d39
gf-espedia <giulia.favaro@espedia.it>
2019-07-18 13:49:33 +0000

    Update in order to work in any portal

--- webapp/controller/TileWorkCenterCalendarView1.controller.js
+++ webapp/controller/TileWorkCenterCalendarView1.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 
 	return Controller.extend("TileWorkCenterCalendar.TileWorkCenterCalendar.controller.TileWorkCenterCalendarView1", {
 		onInit: function () {
-			window.location.replace("https://flpnwc-m9a44f3468.dispatcher.hana.ondemand.com/sites/espediasimplemaintenance#Shell-home ");
+			history.back();
 			window.open("https://workcentercalendar-m9a44f3468.dispatcher.hana.ondemand.com", '_blank');
 		}
 	});
